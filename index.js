c8a37a85a496711fe35349aff33072ad4f5fff78
dervishiamela@gmail.com Dervishi <dervishiamela@gmail.com>
2018-04-02 20:54:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,18 +14,18 @@ function logWhisper(String) {
 
  
 function sayHiToGrandma(string) {
- +  if (string.toLowerCase() === string) {
- +    return "I can't hear you!"
- +  }
- +
- +  if (string.toUpperCase() === string) {
- +    return "YES INDEED!"
- +  }
- +
- +  if (string === "I love you, Grandma.") {
- +    return "I love you, too."
- +  }
- +
- +  return "Are you eating enough?"
- +}
+   if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+   }
+ 
+   if (string.toUpperCase() === string) {
+     return "YES INDEED!"
+   }
+ 
+  if (string === "I love you, Grandma.") {
+     return "I love you, too."
+  }
+ 
+   return "Are you eating enough?"
+ }
       
