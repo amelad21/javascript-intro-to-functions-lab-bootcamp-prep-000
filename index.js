b93ae947b5ac6a50dfcdfb7244940432c4c05101
dervishiamela@gmail.com Dervishi <dervishiamela@gmail.com>
2018-04-02 20:09:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ function shout(string) {
 function logShout(string) {
 console.log(string.toUpperCase())
 }
-function sayHiToGrandma(string){
-return string.toLowerCase("I can't hear you!")
+function logWhisper(String) {
+  console.log(String.toLowerCase())
+}
+
+var uppercase = "YES INDEED!"
+uppercase.toUpperCase('yes indeed') === uppercase
+
+var lowercase = "I can't hear you!"
+lowercase.toLowerCase(" I can't hear you!")
+
+if(function sayHiToGrandma(String){
+return String.toLowerCase("I can't hear you!")}
+else { (function sayHiToGrandma(string) {
+  return string.toUpperCase('yes indeed!')
+} else { (function sayHiToGrandma(string ==='I love you,Grandma') {
+  return string('I love too')
 }
